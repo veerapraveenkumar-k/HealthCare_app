ffbad09ac6bc73e122d2fccbd4a1101f24f90ad7
veerapraveenkumar <veerapraveenkumark@gmail.com>
2025-01-25 22:26:31 +0530

    Adding cors to Server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./config/db.js')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const commonRouter = require('./routes/commonRoutes.js')
 const patoentRouter = require('./routes/patientRoute.js')
@@ -13,6 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/api', commonRouter)
 app.use('/api/patient', patoentRouter)
