46b288c55bb4e94252ae5194335a5a1c35c78021
veerapraveenkumar <veerapraveenkumark@gmail.com>
2025-01-25 23:13:44 +0530

    Update cors in app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost'
   }))
 
 app.use('/api', commonRouter)
