8278d369a7fd3ceebbfb16e8e0b49790640e1148
veerapraveenkumar <veerapraveenkumark@gmail.com>
2025-01-26 16:53:33 +0530

    update cors

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://healthcareappveera.netlify.app'
   }))
 
 app.use('/api', commonRouter)
